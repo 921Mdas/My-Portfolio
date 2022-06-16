41022d5d147bcaffb790192fc22fd02965ba989e
Deo G <66809588+921Mdas@users.noreply.github.com>
2022-06-15 21:18:33 -0400

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="Main">
+      <Navbar />
     </div>
   );
 }
